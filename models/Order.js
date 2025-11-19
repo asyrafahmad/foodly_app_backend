0178f5201326329f7d6efc468627babb403f5ecb
asyrafa <asyrafa@sunway.edu.my>
2025-11-19 12:01:31 +0800

    [Add] Order Model

---
+++ models/Order.js
@@ -0,0 +1,48 @@
+const moongoose = require("mongoose");
+
+const orderItemSchema = new moongoose.Schema({
+    foodId: {
+        type: moongoose.Schema.Types.ObjectId,
+        ref: 'Food'
+    },
+    quantity: {type: Number, default: 1},
+    price: {type: Number, required: true},
+    additives: {type: Array},
+    instructions: {type: String, default: ''},
+
+});
+
+const OrderSchema = new moongoose.Schema({
+
+    userId: {
+        type: moongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    orderItems: [orderItemSchema],
+    orderTotal: {type: Number, required: true},
+    deliveryFee: {type: Number, required: true},
+    grandTotal: {type: Number, required: true},
+    deliveryAddress: {type: moongoose.Schema.Types.ObjectId, ref: 'Address', required: true},
+    restaurantAddress: {type: String, required: true},
+    paymentStatus: {type: String, default: 'Pending', enum: ['Pending', 'Completed', 'Failed']},
+    orderStatus: {type: String, default: 'Pending', enum: ['Placed', 'Preparing', 'Manual', 'Delivered', 'Cancelled', 'Ready', 'Out_for_Delivery']},
+    restaurantId: {
+        type: moongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant'
+    },
+    restaurantCoordinates: [Number], // [longitude, latitude]
+    recipientCoordinates: [Number], // [longitude, latitude]
+    driverId: {
+        type: String,
+        default: ''
+    },
+    rating: {type: Number, min: 1, max: 5, default: 3},
+    feedback: {type: String, default: ''},
+    promoCode: {type: String},
+    discountAmount: {type: Number},
+    notes: {type: String},
+    
+
+}, {timestamps: true});
+
+module.exports = moongoose.model("Order", OrderSchema);
